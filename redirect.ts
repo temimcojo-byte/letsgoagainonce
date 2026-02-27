0cf071b61e203d15a80aafb1b013938460b6f37d
temimcojo-byte <temimcojo@gmail.com>
2026-02-27 06:09:31 +0100

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://ipfs.io/ipfs/bafkreicabkg5owlmtwbbrsyjyon22zgyc64oaejnchg4zq2vpeajn4jcoy"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://ipfs.io/ipfs/bafkreid2vm3c7nvmj6fpdkkexg3ltjhmxjl2vhitogrecnp7sgyyhtpn2i"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
